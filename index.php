2270b2f9122d7d421825b50472bb51a81ef237ee
Rafaelly Camargo <rafaelly.c@aluno.ifsp.edu.br>
2020-12-23 15:43:25 -0200

    Projeto de curriculo em bootstrap

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curriculo Bootstrap</title>
+    <link rel="stylesheet" href="css/estilo.css"></link>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</head>
+<body>
+    <center>
+        <table border=0>
+            <tr>
+                <td>
+                    <div class="card-body text-cor">
+                        <h2 class="card-title letra">Rafaelly Camargo</h5>
+                    </div>
+                  </div>
+                </td>
+            </tr>
+            <tr>
+               
+            <tr>
+                <td colspan="6">
+                    <div class="row justify-content-md-center">
+                        <div class="col col-lg-10">
+                            <text class="h5 letra">Data de nascimento: 03/08/2002</text>
+                        </div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="6">
+                        <div class="row justify-content-md-center">
+                        <div class="col col-lg-10">
+                            <text class="h5 letra">Rua João Pigossi, 50, Vale do Sol, Araraquara-SP</text>
+                        </div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="6">
+                        <div class="row justify-content-md-center">
+                        <div class="col col-lg-10">
+                            <text class="h5 letra">(16)99776-0902</text>
+                        </div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="8">
+                         <div class="row justify-content-md-center">
+                        <div class="col col-lg-10">
+                            <text class="h5 letra">rafa.camargo.info@gmail.com</text>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr rowspan="3">
+                <td> 
+                    <br/>
+                    <br/>
+                    <br/>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <div class="row justify-content-md-center">
+                        <div class="col col-lg-6">
+                            <div class="text-cor">
+                                <text class="h3 letra">Experiência</text>
+                            </div>
+                            <hr>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <div class="row justify-content-md-left">
+                        <div class="col col-lg-6">
+                            <text class="card-text mar"><b>Farmacêutica
+                            </b><div class="text-cor letra1 mar">Nov 2007 - Atual
+                            </div>
+                            </text>
+                            <text class="card-text mar"><b>Farmácia da Vila C
+                            </b><br/>
+                            </text>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <div class="row justify-content-md-center">
+                        <div class="col col-lg-6">
+                            <text class="card-text mar"><b>Farmacêutica
+                            </b><div class="text-cor letra1 mar">Mai 1998 - Nov 2007
+                            </div>
+                            </text>
+                            <text class="card-text mar"><b>Farmácia Alegria
+                            </b><br/>
+                            </text>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr rowspan="3">
+                <td> 
+                    <br/>
+                    <br/>
+                    <br/>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <div class="row justify-content-md-center">
+                        <div class="col col-lg-6">
+                            <div class="text-cor">
+                                <text class="h3 letra">Educacao</text>
+                            </div>
+                            <hr>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <div class="row justify-content-md-left">
+                        <div class="col col-lg-6">
+                            <text class="card-text mar"><b>Universidade de Coimbra
+                            </b><div class="text-cor letra1 mar">O avanco da ciencia eletiva
+                            </div>
+                            <div class="text-cor letra1 mar">Tecnica terapeuticas avancadas
+                            </div>
+                            </text>
+                        
+                        </div>
+                        <div class="col col-lg-6">
+                            <text class="card-text mar"><b>Colégio Filipi
+                            </b><div class="text-cor letra1 mar">Matematica (A)
+                            </div>
+                            <div class="text-cor letra1 mar">Biologia (A)
+                            </div>
+                            <div class="text-cor letra1 mar">Geografia (A)
+                            </div>
+                            </text>
+                        
+                        </div>
+
+                    </div>
+                </td>
+            </tr>
+            <tr>
+               
+        </table>
+    </center>
+</body>
+</html>
